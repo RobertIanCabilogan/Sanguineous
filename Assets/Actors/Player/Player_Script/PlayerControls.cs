bdd4d3ece54bc1d194f5dd2930065d4296008fe9
Robert <156192702+RobertIanCabilogan@users.noreply.github.com>
2025-10-08 15:55:49 +0800

    Added More things

--- Assets/Actors/Player/Player_Script/PlayerControls.cs
+++ Assets/Actors/Player/Player_Script/PlayerControls.cs
@@ -2,38 +2,46 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    private Rigidbody2D pb;
     private Vector2 movement;
     private Vector2 currentVelocity = Vector2.zero;
-    public float jumpVel;
-    public float playerSpeed;
-    
+    public float jumpVel = 10f;
+    public float playerSpeed = 5f;
+    private Vector2 cameraOffset = new Vector2(0, 2);
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Rigidbody2D pb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform Camera;
+
     void Start()
     {
-        pb = GetComponent<Rigidbody2D>();        
+        pb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        if (Input.GetButtonDown("Jump"))
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            Vector2 v = pb.linearVelocity;
-            v.y = jumpVel;
-            pb.linearVelocity = v;
+            pb.linearVelocity = new Vector2(pb.linearVelocity.x, jumpVel);
         }
-        
     }
 
-    private void FixedUpdate()
+    private bool IsGrounded()
     {
-        Vector2 targetVelocity = new Vector2(movement.x, 0f) * playerSpeed;
-        currentVelocity = Vector2.Lerp(currentVelocity, targetVelocity, 0.3f);
-        pb.MovePosition(pb.position + currentVelocity * Time.fixedDeltaTime);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
+    void FixedUpdate()
+    {
+        // Horizontal movement
+        Vector2 targetVelocity = new Vector2(movement.x * playerSpeed, pb.linearVelocity.y);
+        pb.linearVelocity = Vector2.Lerp(pb.linearVelocity, targetVelocity, 0.3f);
 
+        if (Camera != null)
+        {
+            Camera.position = pb.position;
+        }
+    }
 }
