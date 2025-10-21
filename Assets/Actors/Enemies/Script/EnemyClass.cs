acd30bfbb2aa52d10810e830d514118e60f29525
Robert <156192702+RobertIanCabilogan@users.noreply.github.com>
2025-10-21 12:58:20 +0800

    ss

---
+++ Assets/Actors/Enemies/Script/EnemyClass.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class EnemyClass
+{
+    private float health;
+    private float damage;
+    private bool isRanged;
+
+    public EnemyClass(float health, float damage)
+    {
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public EnemyClass(float health, float damage, bool isRanged) : this(health, damage)
+    {
+        this.isRanged = isRanged;
+        this.health = health;
+        this.damage = damage; 
+    }
+
+    public float getHealth()
+    {
+        return health;
+    }
+
+    public float getDamage()
+    {
+        return damage;
+    }
+
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    public bool isDead() 
+    {
+        return health <= 0;
+    }
+      
+}
---
+++ Assets/Actors/Enemies/Script/EnemyScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float Health;
+    public float Damage;
+
+    public EnemyClass meleeEnemy;
+    void Start()
+    {
+        meleeEnemy = new EnemyClass(Health, Damage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
